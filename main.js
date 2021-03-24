2a2e2bff34ba0d5737ed3547635c5c685d8ab4c8
Shawn Carter <53487411+ShawnCart3r@users.noreply.github.com>
2021-03-24 18:45:41 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+//navbar
+const navLinks = document.querySelector('.nav-links');
+const burger = document.querySelector('.hamburger-menu');
+const navbar = document.querySelector('.navbar');
+
+// Event listeners
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active');
+    burger.classList.toggle('bar-change');
+})
+
+// Sticky navbar
+window.addEventListener('scroll', () => {
+    navbar.classList.toggle('sticky', scrollY > 0)
+})
+
+
+//greeting
+let myDate = new Date();
+    let hrs = myDate.getHours();
+
+    let=greetings;
+
+    if (hrs < 12)
+        greet = 'Good Morning';
+    else if (hrs >= 12 && hrs <= 17)
+        greet = 'Good Afternoon';
+    else if (hrs >= 17 && hrs <= 24)
+        greet = 'Good Evening';
+
+    document.getElementById('greetings').innerHTML =
+        '<b>' + greet + '</b>';
+
+
+        //slideshow
+        
